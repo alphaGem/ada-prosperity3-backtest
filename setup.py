4d0515ffbf2b592c450e47ec0a4227cf0533a469
alphagem618 <alphagem618@gmail.com>
2025-03-10 01:47:35 +0100

    set version to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ada-prosperity3-backtest",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=["pandas"],
     entry_points={
